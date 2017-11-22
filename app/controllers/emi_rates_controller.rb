4a1b2143a396db748696c91a832d75103dbc28f3
Nidhi Pandey <nidhi.pandey@accionlabs.com>
2017-11-22 13:04:13 +0530

    Update Emi rates from a dynamic csv

--- app/controllers/emi_rates_controller.rb
+++ app/controllers/emi_rates_controller.rb
@@ -1,6 +1,7 @@
 class EmiRatesController < ApplicationController
 
 	def index
+	  EmiRate.update_rates_from_dynamic_csv
 	  emi_on_amount = EmiRate.where(minimum: params[:amount].to_i)
       emi_rates = emi_on_amount.collect{|emi| {bank: emi.lender, tenures:  
       	          emi_on_amount.where(lender: emi.lender).collect{|rate| {months: 
--- app/models/emi_rate.rb
+++ app/models/emi_rate.rb
@@ -1,2 +1,20 @@
+require 'csv' 
+require 'open-uri' 
+
 class EmiRate < ApplicationRecord
+
+	private
+
+	def self.update_rates_from_dynamic_csv
+	  EmiRate.delete_all
+	  csv_folder_path = Rails.root.join("public/csv").to_s
+      FileUtils.rm_rf(Dir.glob("#{csv_folder_path}/*"))
+      csv_path = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSm2-Ya0gtq144zAkExCrkVTXu_T8NfdvdtMJTJrCYRBbTXo_KUsma7N6ULeV0ihs4e15gDc1vUxoSc/pub?gid=0&single=true&output=csv"
+      File.write(Rails.root.join("public/csv", "emi_rates.csv").to_s, open(csv_path, ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE).read)
+      file = Rails.root.join("public/csv", "emi_rates.csv").to_s
+      CSV.foreach(file, :headers => true) do |row|
+        new_hash = Hash[row.to_hash.map { |k, v| [k.downcase, v] }]
+        EmiRate.create!(new_hash)
+      end
+	end 
 end
